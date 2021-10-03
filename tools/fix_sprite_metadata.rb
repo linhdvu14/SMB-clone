37f622af38c208e47d355342fb9f8fe4f214a45a
Keita Ogawa <sufyl1016@gmail.com>
2021-10-03 01:41:34 +0100

    uncomment

--- tools/fix_sprite_metadata.rb
+++ tools/fix_sprite_metadata.rb
@@ -2,11 +2,11 @@ filename = './Assets/Sprites/4096x3200.png.meta'
 buffer = []
 File.open(filename, 'r') do |f|
   f.each_line do|l|
-    # if l.include?('        x:') || l.include?('        y:') || l.include?('        width:') || l.include?('        height:')
-    #   n = l.split(' ').last.to_i
-    #   puts "original: #{n}, edited: #{n*8}"
-    #   buffer.append(l.gsub(n.to_s, (n*8).to_s))
-    if l.include?('      outline:')
+    if l.include?('        x:') || l.include?('        y:') || l.include?('        width:') || l.include?('        height:')
+      n = l.split(' ').last.to_i
+      puts "original: #{n}, edited: #{n*8}"
+      buffer.append(l.gsub(n.to_s, (n*8).to_s))
+    elsif l.include?('      outline:')
       buffer.append(l.chomp + " []\n")
     elsif l.include?('        - ') || l.include?('      - - ')
       # do not append
